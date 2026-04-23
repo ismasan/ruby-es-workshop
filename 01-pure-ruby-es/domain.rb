4a37b1c3ffc4480dd41589ce261890a21cc3c04d
Ismael Celis <ismaelct@gmail.com>
2026-04-23 14:24:13 +0100

    Append example

--- 01-pure-ruby-es/domain.rb
+++ 01-pure-ruby-es/domain.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative '../lib/mem_store'
-
 BookingStarted = Data.define(:booking_id, :showing_id, :timestamp)
-SeatBooked = Data.define(:booking_id, :seat_id, :timestamp)
+SeatBooked = Data.define(:booking_id, :seat_id, :price, :timestamp)
 BookingPlaced = Data.define(:booking_id, :timestamp)
