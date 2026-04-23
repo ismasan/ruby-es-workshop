320a42dbee28c0bb1491ef3f8d136c003090a499
Ismael Celis <ismaelct@gmail.com>
2026-04-23 22:01:12 +0100

    move evolve and decide and state to domain

--- 01-pure-ruby-es/02-evolve.rb
+++ 01-pure-ruby-es/02-evolve.rb
@@ -2,35 +2,6 @@
 
 require_relative './domain'
 
-class Booking < Struct.new(:status, :booking_id, :showing_id, :seats, keyword_init: true)
-  Seat = Struct.new(:id, :price)
-
-  def self.build
-    new(status: :open, booking_id: nil, showing_id: nil, seats: {})
-  end
-
-  def add_seat(seat_id, price)
-    self.seats[seat_id] = Seat.new(seat_id, price)
-  end
-
-  def total = seats.values.sum(&:price)
-end
-
-def evolve(booking, event)
-  case event
-  when BookingStarted
-    booking.status = :started
-    booking.booking_id = event.booking_id
-    booking.showing_id = event.showing_id
-  when SeatBooked
-    booking.add_seat(event.seat_id, event.price)
-  when BookingPlaced
-    booking.status = :placed
-  end
-
-  booking
-end
-
 booking_stream = []
 
 booking_id = 'booking-1'
