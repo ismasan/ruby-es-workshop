e435b679f89b927481b4930c85831e5c772146c8
Ismael Celis <ismaelct@gmail.com>
2026-04-24 13:01:25 +0100

    fsconsole.rb and memconsole.rb scripts

--- fsconsole.rb
+++ fsconsole.rb
@@ -2,3 +2,5 @@ require_relative 'lib/fs_store'
 require_relative '01-pure-ruby-es/domain'
 
 STORE = FSStore.new
+
+# irb -r ./fsconsole.rb
---
+++ memconsole.rb
@@ -0,0 +1,6 @@
+require_relative 'lib/mem_store'
+require_relative '01-pure-ruby-es/domain'
+
+STORE = MemStore.new
+
+# irb -r ./memoconsole.rb
